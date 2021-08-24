93dda2d0b68f36614704030e1eea3cbabda0da0c
Demian-Wicky <demian.wicky@gmail.com>
2021-08-24 18:08:22 +0200

    dark_trader spec change

--- spec/01_dark_trader_spec.rb
+++ spec/01_dark_trader_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/01_dark_trader'
 
+# Scraper doesn't work yet so.... ¯\_(ツ)_/¯
+
 # describe "the xxxx method" do
 #   it "should rxxxxxxxxx" do
 #     expect(xxxxx(xxxxx)).to eq(xxxxxx)
